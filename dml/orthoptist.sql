71786ac3cfda89b6f3f7e6e8413f7f8527a78c0a
Mathias <mathias.nigsch@gmail.com>
2015-04-19 21:10:50 +0200

    Fixed duplicate entry bug

--- dml/orthoptist.sql
+++ dml/orthoptist.sql
@@ -3,5 +3,4 @@ insert into orthoptist (employee, version) values ('11a1bf06-e16c-48bc-89d4-927c
 insert into orthoptist (employee, version) values ('36056db0-30f3-4b26-ba9d-4bbde324ef26', 0);
 insert into orthoptist (employee, version) values ('b352c867-9b66-418a-b56b-9308398887f1', 17);
 insert into orthoptist (employee, version) values ('18f6bf10-52d9-403a-b97c-fd3fce8b4020', 5);
-insert into orthoptist (employee, version) values ('b352c867-9b66-418a-b56b-9308398887f1', 4);
 insert into orthoptist (employee, version) values ('40c5d5d2-a007-4014-a939-ffa6968ce9af', 4);
