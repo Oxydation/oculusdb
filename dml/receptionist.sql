62037e46e6a34e475a04d0d351114af7ce4ffc4e
Mathias <mathias.nigsch@gmail.com>
2015-04-19 18:42:38 +0200

    Added new fill data for tables Updated Descriptions.txt

---
+++ dml/receptionist.sql
@@ -0,0 +1,8 @@
+insert into receptionist (employee, version) values ('ad01032c-c7d1-4423-887b-57c4800472a9', 3);
+insert into receptionist (employee, version) values ('68a96756-1f36-4b8d-96a1-143dc526b9e5', 12);
+insert into receptionist (employee, version) values ('f176117a-8738-4646-a80e-6f92424212d9', 14);
+insert into receptionist (employee, version) values ('ad01032c-c7d1-4423-887b-57c4800472a9', 6);
+insert into receptionist (employee, version) values ('f0efa094-968b-4f57-b48c-c765b3df39ba', 14);
+insert into receptionist (employee, version) values ('86406c98-60bf-4a7a-92dc-cd2555e01d93', 3);
+insert into receptionist (employee, version) values ('36056db0-30f3-4b26-ba9d-4bbde324ef26', 5);
+insert into receptionist (employee, version) values ('e4952572-4e53-4d32-8b08-5a6965f328e5', 2);
--- fillDB.sql
+++ fillDB.sql
@@ -2,3 +2,5 @@
 SOURCE ./dml/employee.sql
 SOURCE ./dml/patient.sql
 SOURCE ./dml/doctor.sql
+SOURCE ./dml/orthoptist.sql
+SOURCE ./dml/receptionist.sql
