5e8ae4603c8024ca68eaddf41aa543fb606cb1bf
Mathias <mathias.nigsch@gmail.com>
2015-04-19 20:47:06 +0200

    Added new testfiles to fillDB.sql Added descriptions

--- fillDB.sql
+++ fillDB.sql
@@ -4,3 +4,13 @@ SOURCE ./dml/patient.sql
 SOURCE ./dml/doctor.sql
 SOURCE ./dml/orthoptist.sql
 SOURCE ./dml/receptionist.sql
+SOURCE ./dml/insurance.sql
+SOURCE ./dml/drug.sql
+SOURCE ./dml/user.sql
+SOURCE ./dml/calendarentry.sql
+SOURCE ./dml/appointment.sql
+SOURCE ./dml/anamnesis.sql
+SOURCE ./dml/insurance.sql
+SOURCE ./dml/queue.sql
+SOURCE ./dml/queue_employee.sql
+SOURCE ./dml/queueentry.sql
