f2e4ffca7a06b1464c7922004fa9d310692bdbff
Mathias <mathias.nigsch@gmail.com>
2015-04-19 22:06:38 +0200

    Updated batch scripts

--- BatchScripts/Group1 (A-C)/teamB_updateDB.bat
+++ BatchScripts/Group1 (A-C)/teamB_updateDB.bat
@@ -2,4 +2,5 @@ rmdir /S /Q C:\Users\Administrator\Documents\oculusdb_group1
 git clone https://github.com/Oxydation/oculusdb.git C:\Users\Administrator\Documents\oculusdb_group1
 cd C:\Users\Administrator\Documents\oculusdb_group1
 C:\xampp\mysql\bin\mysql.exe --default-character-set=utf8 --user=root --database=oculus_b < .\oculusdb_wb.sql
-C:\xampp\mysql\bin\mysql.exe --default-character-set=utf8 --user=root --database=oculus_b < .\fillDB.sql
+C:\xampp\mysql\bin\mysql.exe --default-character-set=utf8 --debug-info --user=root --database=oculus_b < .\fillDB.sql
+pause
