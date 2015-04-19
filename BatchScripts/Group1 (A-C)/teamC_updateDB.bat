1204b9a78d065d00afe2e8004c36f79c6fbb1f6e
Mathias <mathias.nigsch@gmail.com>
2015-04-19 22:21:13 +0200

    Added batch scripts for team A and C - used on okeanos server

---
+++ BatchScripts/Group1 (A-C)/teamC_updateDB.bat
@@ -0,0 +1,6 @@
+rmdir /S /Q C:\Users\Administrator\Documents\oculusdb_group1
+git clone https://github.com/Oxydation/oculusdb.git C:\Users\Administrator\Documents\oculusdb_group1
+cd C:\Users\Administrator\Documents\oculusdb_group1
+C:\xampp\mysql\bin\mysql.exe --default-character-set=utf8 --user=root --database=oculus_c < .\oculusdb_wb.sql
+C:\xampp\mysql\bin\mysql.exe --default-character-set=utf8 --debug-info --user=root --database=oculus_c < .\fillDB.sql
+pause
